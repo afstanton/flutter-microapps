a58c2bd163538b448f2121b6af8203d513b84453
Aaron F Stanton <afstanton@gmail.com>
2024-07-12 16:05:07 -0400

    Burning it down and starting over

--- file_picker_example/lib/main.dart
+++ file_picker_example/lib/main.dart
@@ -1,120 +1,125 @@
 import 'package:flutter/material.dart';
-import 'dart:typed_data';
-import 'package:flutter/foundation.dart' show kIsWeb;
-import 'package:universal_html/html.dart' as html;
-import 'file_selector_service.dart';
 
 void main() {
-  runApp(const FilePickerExample());
+  runApp(const MyApp());
 }
 
-class FilePickerExample extends StatelessWidget {
-  const FilePickerExample({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: FilePickerHome(fileSelectorService: FileSelectorService()),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class FilePickerHome extends StatefulWidget {
-  final FileSelectorService fileSelectorService;
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
 
-  const FilePickerHome({super.key, required this.fileSelectorService});
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
 
   @override
-  _FilePickerHomeState createState() => _FilePickerHomeState();
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _FilePickerHomeState extends State<FilePickerHome> {
-  Uint8List? fileContent;
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        title: const Text('File Picker Example'),
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
       ),
       body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
         child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          //
+          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+          // action in the IDE, or press "p" in the console), to see the
+          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            ElevatedButton(
-              onPressed: _readFile,
-              child: const Text('Read'),
+            const Text(
+              'You have pushed the button this many times:',
             ),
-            ElevatedButton(
-              onPressed: fileContent == null ? null : _writeFile,
-              child: const Text('Write'),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
             ),
           ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
-  }
-
-  Future<void> _readFile() async {
-    final file = await widget.fileSelectorService.openFileDialog();
-    if (file != null) {
-      final content = await file.readAsBytes();
-      setState(() {
-        fileContent = content;
-      });
-    }
-  }
-
-  Future<void> _writeFile() async {
-    if (fileContent == null) return;
-
-    if (kIsWeb) {
-      final filename = await _promptFileName();
-      if (filename != null) {
-        _downloadFileWeb(fileContent!, filename);
-      }
-    } else {
-      final saveLocation =
-          await widget.fileSelectorService.getSaveLocationDialog();
-      if (saveLocation != null) {
-        await widget.fileSelectorService
-            .saveFile(fileContent!, saveLocation.path);
-      }
-    }
-  }
-
-  Future<String?> _promptFileName() async {
-    return showDialog<String>(
-      context: context,
-      builder: (BuildContext context) {
-        String fileName = '';
-        return AlertDialog(
-          title: const Text('Enter file name'),
-          content: TextField(
-            onChanged: (value) {
-              fileName = value;
-            },
-            decoration: const InputDecoration(hintText: "File name"),
-          ),
-          actions: <Widget>[
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop(fileName);
-              },
-              child: const Text('Save'),
-            ),
-          ],
-        );
-      },
-    );
-  }
-
-  void _downloadFileWeb(Uint8List data, String filename) {
-    final blob = html.Blob([data]);
-    final url = html.Url.createObjectUrlFromBlob(blob);
-    html.AnchorElement(href: url)
-      ..setAttribute('download', filename)
-      ..click();
-    html.Url.revokeObjectUrl(url);
   }
 }
--- file_picker_example/test/widget_test.dart
+++ file_picker_example/test/widget_test.dart
@@ -1,79 +1,30 @@
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility in the flutter_test package. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:file_picker_example/main.dart';
-import 'package:file_picker_example/file_selector_service.dart';
-import 'package:mockito/mockito.dart';
-import 'dart:typed_data';
-import 'package:file_selector/file_selector.dart';
 
-class MockFileSelectorService extends Mock implements FileSelectorService {}
+import 'package:file_picker_example/main.dart';
 
 void main() {
-  late MockFileSelectorService mockFileSelectorService;
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const MyApp());
 
-  setUp(() {
-    mockFileSelectorService = MockFileSelectorService();
-  });
+    // Verify that our counter starts at 0.
+    expect(find.text('0'), findsOneWidget);
+    expect(find.text('1'), findsNothing);
 
-  testWidgets('Initial state of the app', (WidgetTester tester) async {
-    await tester.pumpWidget(MaterialApp(
-      home: FilePickerHome(fileSelectorService: mockFileSelectorService),
-    ));
+    // Tap the '+' icon and trigger a frame.
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.pump();
 
-    // Verify the initial state
-    expect(find.text('Read'), findsOneWidget);
-    expect(find.text('Write'), findsOneWidget);
-    final writeButton =
-        tester.widget<ElevatedButton>(find.byType(ElevatedButton).last);
-    expect(writeButton.onPressed, isNull);
-  });
-
-  testWidgets('Read button enables Write button after file selection',
-      (WidgetTester tester) async {
-    final testFileContent = Uint8List.fromList('Hello, world.'.codeUnits);
-    final mockXFile = XFile.fromData(testFileContent);
-
-    when(mockFileSelectorService.openFileDialog())
-        .thenAnswer((_) async => mockXFile);
-
-    await tester.pumpWidget(MaterialApp(
-      home: FilePickerHome(fileSelectorService: mockFileSelectorService),
-    ));
-
-    // Simulate tapping the Read button
-    await tester.tap(find.text('Read'));
-    await tester.pumpAndSettle();
-
-    // Verify the Write button is enabled after reading a file
-    final writeButton =
-        tester.widget<ElevatedButton>(find.byType(ElevatedButton).last);
-    expect(writeButton.onPressed, isNotNull);
-  });
-
-  testWidgets('Write button saves file', (WidgetTester tester) async {
-    final testFileContent = Uint8List.fromList('Hello, world.'.codeUnits);
-    final mockXFile = XFile.fromData(testFileContent);
-    final mockSaveLocation = FileSaveLocation('/path/to/output.txt');
-
-    when(mockFileSelectorService.openFileDialog())
-        .thenAnswer((_) async => mockXFile);
-
-    when(mockFileSelectorService.getSaveLocationDialog())
-        .thenAnswer((_) async => mockSaveLocation);
-
-    await tester.pumpWidget(MaterialApp(
-      home: FilePickerHome(fileSelectorService: mockFileSelectorService),
-    ));
-
-    // Simulate tapping the Read button
-    await tester.tap(find.text('Read'));
-    await tester.pumpAndSettle();
-
-    // Simulate tapping the Write button
-    await tester.tap(find.text('Write'));
-    await tester.pumpAndSettle();
-
-    // Verify the file was saved
-    verify(mockFileSelectorService.getSaveLocationDialog()).called(1);
+    // Verify that our counter has incremented.
+    expect(find.text('0'), findsNothing);
+    expect(find.text('1'), findsOneWidget);
   });
 }
